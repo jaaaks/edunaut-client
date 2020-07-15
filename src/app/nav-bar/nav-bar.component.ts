5ac9c90592c28a5ac1115c148db6c1077df190dd
saurav137 <k123saurav@gmail.com>
2020-07-15 22:19:03 +0530

    login-register improved

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgwWowService } from 'ngx-wow';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private wowService: NgwWowService) {
+  
+  }
+
 
   ngOnInit(): void {
   }
