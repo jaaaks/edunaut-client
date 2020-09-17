c09fa8e7c8c6e324bff22d689692fe57f33ac914
Anshul Kumar <warofanshul@gmail.com>
2020-09-17 23:06:00 +0530

    Completed Course Detail Page- Functionality Wise

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit,AfterViewChecked,ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ViewChild } from '@angular/core';
 import { CourseDetailService } from '../services/course-detail.service';
-import {ModalDirective} from 'angular-bootstrap-md';
+import { ModalDirective } from 'angular-bootstrap-md';
 import { AngularFireAuth } from "@angular/fire/auth";
-import {ActivatedRoute} from '@angular/router';
-import {MessageService} from '../services/message.service';
-import {HttpParams} from '@angular/common/http';
-import * as $ from 'jquery';
-import { textChangeRangeIsUnchanged } from 'typescript';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService } from '../services/message.service';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+import { EmailVerificationComponent } from '../email-verification/email-verification.component';
+import { SeacrhServiceService } from '../services/seacrh-service.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
@@ -29,7 +31,7 @@ export class CourseDetailComponent implements OnInit {
   course;
   isCourse;
   userId = "";
-  reviews ;
+  reviews;
   reviewNo;
   averageReview;
   isReview;
@@ -37,21 +39,24 @@ export class CourseDetailComponent implements OnInit {
   courseDomain;
   domain;
   subdomain;
+  isBookmark = false;
+  userData;
+  professor: Array<{ name: String, ins: String, desc: String }>;
   count = 0;
-  constructor(private courseService: CourseDetailService, private route: ActivatedRoute, private afauth:AngularFireAuth, private messageService:MessageService) { 
+  constructor(private snackBar: MatSnackBar, private searchService: SeacrhServiceService, private dialog: MatDialog, private courseService: CourseDetailService, private route: ActivatedRoute, private afauth: AngularFireAuth, private messageService: MessageService) {
     this.afauth.authState.subscribe(
       res => {
-        this.userId= res.uid;   
-        
+        this.userId = res.uid;
+        this.userData = res;
       });
-}
+  }
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe((params)=>{
-      this.courseId={...params};
+    this.route.queryParamMap.subscribe((params) => {
+      this.courseId = { ...params };
       this.courseId = this.courseId.params.cid;
     })
-    this.courseService.getCourseById([this.courseId]).subscribe((data)=>{
-      
+    this.courseService.getCourseById([this.courseId]).subscribe((data) => {
+
       this.course = data[0];
       this.isCourse = this.course.course_id;
       this.domain = this.course.course_field.split(', ');
@@ -59,47 +64,49 @@ export class CourseDetailComponent implements OnInit {
       this.courseDomain = this.domain.concat(this.subdomain);
       console.log(this.courseDomain)
       console.log(data);
-      })
-    this.courseService.getReviewById(this.courseId).subscribe((data)=>{
+    })
+    this.courseService.getReviewById(this.courseId).subscribe((data) => {
       console.log(data);
       this.reviews = data;
       this.reviewNo = this.reviews.length;
       var total = 0;
       console.log(this.userId);
-      for(var i = 0; i < this.reviewNo; i++) {
+      for (var i = 0; i < this.reviewNo; i++) {
         total += this.reviews[i].rating;
-        this.reviews[i].rdate= Date.parse(this.reviews[i].rdate);
+        this.reviews[i].rdate = Date.parse(this.reviews[i].rdate);
         console.log(this.reviews[i].rdate);
-    }
-    this.averageReview = total/this.reviewNo; 
-    this.reviewTime();
+      }
+      this.averageReview = total / this.reviewNo;
+      this.reviewTime();
     })
-    
-    
+
+
   }
-  ngDoCheck(): void{
-    if(this.count ==0){if(this.userId && this.reviews.length){
-    this.isReview = this.reviews.find(({id}) => id === this.userId);
-    this.userReview = this.isReview.review;
-    this.currentRate = this.isReview.rating;
-    this.count++;
-  }}
+  ngDoCheck(): void {
+    if (this.count == 0) {
+      if (this.userId && this.isReview) {
+        this.isReview = this.reviews.find(({ id }) => id === this.userId);
+        this.userReview = this.isReview.review;
+        this.currentRate = this.isReview.rating;
+        this.count++;
+      }
+    }
   }
-  lowestRating(){
-    this.reviews.sort(function(a,b){return a.rating - b.rating});
+  lowestRating() {
+    this.reviews.sort(function (a, b) { return a.rating - b.rating });
   }
-  reviewTime(){
+  reviewTime() {
     let curDate = Date.now();
-    for(var i = 0; i < this.reviewNo; i++){
+    for (var i = 0; i < this.reviewNo; i++) {
       this.reviews[i].rdate = curDate - this.reviews[i].rdate;
       this.reviews[i].rdate = this.timeConversion(this.reviews[i].rdate);
     }
   }
-  highestRating(){
-    this.reviews.sort(function(a,b){return b.rating - a.rating});
+  highestRating() {
+    this.reviews.sort(function (a, b) { return b.rating - a.rating });
   }
-  latestDate(){
-    this.reviews.sort(function(a,b){return b.rid - a.rid});
+  latestDate() {
+    this.reviews.sort(function (a, b) { return b.rid - a.rid });
   }
 
   timeConversion(millisec) {
@@ -112,60 +119,126 @@ export class CourseDetailComponent implements OnInit {
 
     var days = (millisec / (1000 * 60 * 60 * 24));
     if (seconds < 60) {
-      if(seconds<2)
-        return   Math.trunc(seconds) + " second";
-        return Math.trunc(seconds) + " seconds";
+      if (seconds < 2)
+        return Math.trunc(seconds) + " second";
+      return Math.trunc(seconds) + " seconds";
     } else if (minutes < 60) {
-      if(minutes<2)
-        return   Math.trunc(minutes) + " min";
-        return  Math.trunc(minutes) + " mins";
+      if (minutes < 2)
+        return Math.trunc(minutes) + " min";
+      return Math.trunc(minutes) + " mins";
     } else if (hours < 24) {
-      if(hours<2)
-        return   Math.trunc(hours) + " hour";
-        return Math.trunc(hours) + " hours";
+      if (hours < 2)
+        return Math.trunc(hours) + " hour";
+      return Math.trunc(hours) + " hours";
     } else {
-      if(days<2)
-        return   Math.trunc(days) + " day";
+      if (days < 2)
+        return Math.trunc(days) + " day";
       return Math.trunc(days) + " days";
-    }
-}
-
-  onPost(data){
-    if(this.userId){
-      if(this.currentRate<1){
-        alert("Please rate this course.")
-      }
-      else{
-    this.courseService.writeCourseReview(
-      {
-        courseid:this.courseId,
-        review:data.review,
-        rating:this.currentRate,
-        userid:{
-            uid: this.userId
-        }
-      }
-    );}}
-    else{
-      alert("Please log in to give your review");
     }
   }
 
-  onEdit(data){
+  onPost(data) {
+    if (this.userId) {
+      if (this.currentRate < 1) {
+        alert("Please rate this course.")
+      }
+      else {
+        this.courseService.writeCourseReview(
+          {
+            courseid: this.courseId,
+            review: data.review,
+            rating: this.currentRate,
+            userid: {
+              uid: this.userId
+            }
+          }
+        );
+      }
+    }
+    else {
+      const dialogRef = this.dialog.open(LoginComponent, {
+        height: '520px',
+        minWidth: '411px',
+        position: {
+          top: '15vh',
+        },
+        disableClose: true
+      }
+      );
+    }
+  }
+
+  onEdit(data) {
     this.courseService.updateReview(
       {
         rid: this.isReview.rid,
         review: this.userReview,
         rating: this.currentRate
       }
-    ).subscribe((res)=>{
+    ).subscribe((res) => {
       location.reload();
     });
   }
-  comparator(){
+  comparator() {
     console.log();
-   this.messageService.addToCompare(this.course);
+    this.messageService.addToCompare(this.course);
   }
- 
+
+  bookmarkCourse() {
+    if (this.userId) {
+      if (this.userData.emailVerified) {
+        this.snackBar.open('Course Bookmarked', 'close', {
+          duration: 6000
+        })
+        this.searchService.bookMarkcourse(
+          {
+            courseid: this.courseId,
+            status: 0,
+            percentage: 0,
+            userid: {
+              uid: this.userId
+            }
+          }
+        ).subscribe(data => {
+          this.isBookmark= true;
+        }, err => {
+          if (err = 'success') {
+            this.snackBar.open('Course BookMarked', 'close', {
+              duration: 6000,
+            })
+            this.isBookmark = true;
+          }
+        })
+      } else {
+        const dialogRef1 = this.dialog.open(EmailVerificationComponent, {
+          height: '520px',
+          minWidth: '411px',
+          position: {
+            top: '15vh',
+          },
+
+        }
+        );
+        this.userData.sendEmailVerification().then(result => {
+          this.isBookmark = true;
+          dialogRef1.close();
+        },
+          err => {
+            console.log('not verified')
+          })
+      }
+    }
+    else {
+      const dialogRef = this.dialog.open(LoginComponent, {
+        height: '520px',
+        minWidth: '411px',
+        position: {
+          top: '15vh',
+        },
+        disableClose: true
+      }
+      );
+    }
+  }
 }
 
